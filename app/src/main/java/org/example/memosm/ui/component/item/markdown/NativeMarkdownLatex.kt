0fcb029647d806c29fa6058fec0f7bb69cca4c1b
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2026-02-04 20:44:23 -0600

    Add dark theme support

--- app/src/main/java/org/example/memosm/ui/component/item/markdown/NativeMarkdownLatex.kt
+++ app/src/main/java/org/example/memosm/ui/component/item/markdown/NativeMarkdownLatex.kt
@@ -1,18 +1,23 @@
 package org.example.memosm.ui.component.item.markdown
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
 import com.agog.mathdisplay.MTMathView
 
 @Composable
 fun NativeMarkdownLatex(
-    latex: String, modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    latex: String,
 ) {
+    val c = MaterialTheme.colorScheme.onSurface.toArgb()
     AndroidView(
         factory = { context ->
         MTMathView(context, null).apply {
             fontSize = 36f
+            textColor = c
             // labelMode = MTMathViewMode.KMathViewModeDisplay
         }
     }, update = { view ->
