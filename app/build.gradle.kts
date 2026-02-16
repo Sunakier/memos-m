537e93e021e7e2278daaa8d14720bc11f6c938ad
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2026-02-16 13:53:09 -0600

    Add testContainers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -183,6 +183,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    testImplementation(libs.testcontainers)
 
 
     // ROOM
